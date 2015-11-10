51a353deffc56041bd35f4f1d8da6371ed91f62e
Sai Kishore <sai.kishu@gmail.com>
2015-11-10 14:28:37 -0800

    traditional computing model

---
+++ traditional/RandomFileGenerator.java
@@ -0,0 +1,52 @@
+/**
+Simple Random File Generator
+Usage: java RandomFileGenerator <buffer size in MB>
+Max size: ((Integer.MAX_VALUE-5)/(1024*1024)) MB
+**/
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class RandomFileGenerator {
+
+  public static final int MEGA_MULTIPLIER = 1048576;
+
+  public static void main(String[] args) {
+      long startTime = System.currentTimeMillis();
+      System.out.println("Start time: " + startTime);
+
+      if(args.length == 0) return;
+
+      int numBytes = Integer.parseInt(args[0]);
+      byte[] buffer = new byte[numBytes * MEGA_MULTIPLIER];
+
+      //write to file
+      File file;
+      FileOutputStream fop = null;
+      try {
+        file = new File("data.bin"); //data file
+        fop = new FileOutputStream(file);
+        if (!file.exists()) {
+  				file.createNewFile();
+  			}
+        fop.write(buffer);
+        fop.flush();
+        fop.close();
+      } catch (IOException e) {
+			     e.printStackTrace();
+		  } finally {
+    			try {
+    				if (fop != null) {
+    					fop.close();
+    				}
+    			} catch (IOException e) {
+    				e.printStackTrace();
+    			}
+		  }
+      long endTime = System.currentTimeMillis();
+      System.out.println("End time: " + endTime);
+
+      System.out.println("Process took: " + (endTime - startTime)/1000.0 + " seconds");
+  }
+
+}
---
+++ traditional/RandomFileReader.java
@@ -0,0 +1,45 @@
+/**
+Simple Random File Reader - Sequential read
+Usage: java RandomFileReader file_name
+**/
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.FileNotFoundException;
+
+public class RandomFileReader {
+
+  public static final int MEGA_MULTIPLIER = 1048576;
+
+  public static void main(String[] args) {
+      long startTime = System.currentTimeMillis();
+
+      if(args.length == 0) return;
+
+      String fileName = args[0];
+      byte[] buffer = new byte[MEGA_MULTIPLIER]; // 1 MB at a time
+
+      //read from file
+      FileInputStream fin = null;
+      try {
+        fin = new FileInputStream(fileName);
+        int total = 0;
+        int bytesRead = 0;
+        while((bytesRead = fin.read(buffer)) != -1 ) {
+          System.out.println(new String(buffer));
+          total += bytesRead;
+        }
+        fin.close();
+        System.out.println("Read bytes: " + total);
+      } catch (FileNotFoundException e) {
+			     e.printStackTrace();
+		  } catch (IOException e) {
+    				e.printStackTrace();
+    	}
+
+      long endTime = System.currentTimeMillis();
+      System.out.println("Start time: " + startTime);
+      System.out.println("End time: " + endTime);
+      System.out.println("Process took: " + (endTime - startTime)/1000.0 + " seconds");
+  }
+
+}
